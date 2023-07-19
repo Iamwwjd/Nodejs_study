eafa6d67e7ccb15d7094cb1a6bf13e8d96037e4c
Iamwwjd <iamxxjd@gmail.com>
2023-07-19 09:05:17 +0900

    update

--- main.js
+++ main.js
@@ -27,7 +27,10 @@ var app = http.createServer(function(request,response){ // 서버 생성
         topic.update_process(request, response);
     } else if(pathname === '/delete_process'){
         topic.delete_process(request. response);
-    } else {
+    } else if(pathname === '/author'){
+      author.home(request. response);
+    }
+    else {
       response.writeHead(404);
       response.end('Not found');
     }
