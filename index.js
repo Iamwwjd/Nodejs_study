7a17d7cb19725d846f815c59700345c2e63aeea0
Iamwwjd <iamxxjd@gmail.com>
2023-07-23 22:44:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const Router = require("./router");
+app.use(express.json())
 app.use ("/router", Router)
 
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true}))
 app.listen (3000)
